042e21625173743518c2f9fa2ab830617ab11efd
IkerMTdev <183281249+IkerMTdev@users.noreply.github.com>
2025-02-05 15:37:00 +0100

    cambio en el CopyRight del footer

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -32,8 +32,8 @@ const Footer = () => {
             </li>
           </ul>
         </div>
-        <hr class="my-6 border-gray-200 sm:mx-auto lg:my-8" />
-        <span class="block text-sm sm:text-center">
+        <hr class="my-4 md:my-6 lg:my-8" />
+        <span class="block text-sm text-center">
           © 2025{" "}
           <a href="https://flowbite.com/" class="hover:underline">
             BlinkURL™
